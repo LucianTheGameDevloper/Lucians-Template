1928fa77a8516d5f023d2f069388a04ee96e2371
LucianTheGameDevloper <113057110+LucianTheGameDevloper@users.noreply.github.com>
2023-07-14 23:51:51 -0400

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("Galactic Paid Menu")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("Galactic Paid Menu")]
+[assembly: AssemblyTitle("Galactic Paid Menu")]
+[assembly: AssemblyVersion("1.0.0.0")]
